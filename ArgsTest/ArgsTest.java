aaac46bf5a586eafcd86289251b542220fd35633
Abdullahi Bashiru B. <75587876+White-Kabar@users.noreply.github.com>
2021-01-22 00:32:53 -0800

    Update

---
+++ ArgsTest/ArgsTest.java
@@ -0,0 +1,21 @@
+
+public class ArgsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int j = 0;
+		int g = Add(j);
+		System.out.println(g);
+	}
+	public static int Add(int o) {
+		int sum, a, b, c, e, f;
+		int [] h = {2, 4, 6, 8, 10};
+		a = h[0]; b = h[1]; c = h[2];
+		e = h[3]; f = h[4];
+		sum = a+b+c+e+f;
+		return sum;
+		 
+		
+	}
+}
+
